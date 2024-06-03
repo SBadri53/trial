8c28c37f9e7e179898bc8897bca7ef25c124c876
S Badri <122665111+SBadri53@users.noreply.github.com>
2024-06-03 14:07:55 +0530

    M1

--- resources/views/qrcode1.blade.php
+++ resources/views/qrcode1.blade.php
@@ -1,7 +1,7 @@
 <!--<img src="data:image/png;base64,C:\qrcode_1716020593.png" />-->
 <div>
    <h4>Hostinger successfull</h4>
-   <h4>TEST</h4>
+   
    VALUE of var 1 :  {{ $variable1 }}
    <img src="{{ $src1 }}" alt="src1">
    <img src="data:image/png;base64,". {{   $imageData  }}  >
